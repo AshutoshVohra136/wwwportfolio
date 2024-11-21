e2baa162007a2463f8f7d63602b4fd43697a81e2
AshutoshVohra <ashutoshvohra136@gmail.com>
2024-11-21 12:58:56 +0530

    okay

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+// const express = require("express");
+
+// require("dotenv").config();
+// const app = express();
+// const path = require("path");
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+// app.listen(process.env.PORT || 4040, () =>
+//   console.log(`running on Port : ${process.env.PORT}`)
+// );
+
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const path = require("path");
-const port = 5000;
 
-app.use(express.static(path.join(__dirname, "public")));
+// Serve video from the public folder
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
+// Serve the index.html file for the root route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
-app.listen(port, () => console.log(`running on Port : ${port}`));
+
+module.exports = app;
