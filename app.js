6272f6c3ae27f7e35aae4a0eea5854cb1e7ef0e6
AshutoshVohra <ashutoshvohra136@gmail.com>
2024-11-22 21:33:14 +0530

    pro

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-// const express = require("express");
-
-// require("dotenv").config();
-// const app = express();
-// const path = require("path");
-
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
-// app.listen(process.env.PORT || 4040, () =>
-//   console.log(`running on Port : ${process.env.PORT}`)
-// );
-
 const express = require("express");
+
+require("dotenv").config();
+const app = express();
 const path = require("path");
 
-const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
-// Serve video from the public folder
-app.use("/public", express.static(path.join(__dirname, "../public")));
-
-// Serve the index.html file for the root route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
+app.listen(process.env.PORT || 4040, () =>
+  console.log(`running on Port : ${process.env.PORT}`)
+);
 
-module.exports = app;
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+
+// // Serve video from the public folder
+// app.use("/public", express.static(path.join(__dirname, "../public")));
+
+// // Serve the index.html file for the root route
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../index.html"));
+// });
+
+// module.exports = app;
